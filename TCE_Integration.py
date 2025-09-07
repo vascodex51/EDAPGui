72d3746f8430aa51850f5e73ec56ac4b3bc58c5c
Steve Towner <stevetowner@yahoo.com>
2025-09-07 09:59:16 -0500

    Separated TCE to separate tab.

--- TCE_Integration.py
+++ TCE_Integration.py
@@ -1,5 +1,9 @@
+import json
 import os
 import sqlite3
+import webbrowser
+import tkinter as tk
+from tkinter import ttk
 from typing import Union, Any, final
 
 from EDlogger import logger
@@ -10,16 +14,26 @@ class TceIntegration:
     """ Handles TCE Integration. """
 
     def __init__(self, ed_ap, cb):
+        self.ap_gui = None
         self.ap = ed_ap
         self.ap_ckb = cb
-        self.tce_path = self.ap.config['TCEInstallationPath']  # i.e. C:\TCE
-        self.tce_destination_filepath = self.tce_path + "\\DUMP\\Destination.json"
+        self.tce_installation_path = self.ap.config['TCEInstallationPath']  # i.e. C:\TCE
+        self.tce_shoppinglist_path = self.tce_installation_path + "\\SHOP\\ED_AP Shopping List.tsl"
+        self.tce_destination_filepath = self.tce_installation_path + "\\DUMP\\Destination.json"
+        self.var_tce_installation_path = tk.StringVar()
+        self.var_tce_shoppinglist_path = tk.StringVar()
+        self.var_tce_destination_filepath = tk.StringVar()
+
+        # Update GUI
+        self.var_tce_installation_path.set(self.ap.config['TCEInstallationPath'])
+        self.var_tce_shoppinglist_path.set(self.tce_shoppinglist_path)
+        self.var_tce_destination_filepath.set(self.ap.config['TCEDestinationFilepath'])
 
     def write_shopping_list(self) -> bool:
         """ Write the global shopping list to file.
         @return: True once complete.
         """
-        db_path = self.tce_path + "\\DB\\Resources.db"
+        db_path = self.tce_installation_path + "\\DB\\Resources.db"
         public_goods = self.read_resources_db(db_path, 'public_Goods')
         if public_goods is None:
             return False
@@ -31,8 +45,7 @@ class TceIntegration:
         if global_buy_commodities is None:
             return False
 
-        output_filepath = self.tce_path + "\\SHOP\\ED_AP Shopping List.tsl"
-        with open(output_filepath, "w") as file:
+        with open(self.tce_shoppinglist_path, "w") as file:
             file.write("<TCE_ShoppingList>\r\n")
             file.write("   <ShoppingListItems\r\n")
 
@@ -47,8 +60,8 @@ class TceIntegration:
                 for i, commodity in enumerate(global_buy_commodities):
                     qty = global_buy_commodities[commodity]
                     # if qty > 0:
-                    print(f"Need {commodity}: {global_buy_commodities[commodity]}")
                     if good['Tradegood'].upper() == commodity.upper():
+                        # print(f"Need {commodity}: {global_buy_commodities[commodity]}")
                         good_id = good['ID']
                         name = good['Tradegood'].upper()
                         price = good['AvgPrice']
@@ -114,6 +127,87 @@ class TceIntegration:
         data = self.fetch_data_as_dict(filepath, query)
         # print(data)
         return data
+
+    def create_gui_tab(self, ap_gui, tab):
+        self.ap_gui = ap_gui
+        tab.columnconfigure(0, weight=1)
+
+        # Description
+        blk_desc = ttk.LabelFrame(tab, text="Description")
+        blk_desc.grid(row=1, column=0, padx=5, pady=5, sticky="NSEW")
+
+        lbl_tce_desc = ttk.Label(blk_desc, text="The Trade Computer Extension Mk.II is a 3rd party app for Elite "
+                                                "Dangerous and supports the player in many ways.")
+        lbl_tce_desc.grid(row=1, column=0, padx=5, pady=5, sticky="EW")
+        lbl_tce_desc1 = ttk.Label(blk_desc, text="It's an overlay, that displays on top of the HUD of Elite "
+                                                 "Dangerous, so it's not necessary to ALT-TAB away from the game.")
+        lbl_tce_desc1.grid(row=2, column=0, padx=5, pady=5, sticky="EW")
+
+        btn_tce_web = ttk.Button(blk_desc, text='Trade Computer Extension (TCE) forum page',
+                                 command=self.goto_tce_webpage)
+        btn_tce_web.grid(row=3, column=0, padx=5, pady=5, sticky="W")
+
+        # Options
+        blk_options = ttk.LabelFrame(tab, text="Options")
+        blk_options.grid(row=2, column=0, padx=5, pady=5, sticky="NSEW")
+        blk_options.columnconfigure(1, weight=1)
+
+        lbl_tce_inst = ttk.Label(blk_options, text='TCE Installation Folder (i.e. C:\\TCE)')
+        lbl_tce_inst.grid(row=3, column=0, padx=5, pady=5, sticky="NSEW")
+        txt_tce_inst = ttk.Entry(blk_options, textvariable=self.var_tce_installation_path)
+        txt_tce_inst.bind('<FocusOut>', self.entry_update)
+        txt_tce_inst.grid(row=3, column=1, padx=5, pady=5, sticky="NSEW")
+
+        lbl_tce_dest = ttk.Label(blk_options, text='TCE Dest json:')
+        lbl_tce_dest.grid(row=4, column=0, padx=5, pady=5, sticky="NSEW")
+        txt_tce_dest = ttk.Entry(blk_options, textvariable=self.var_tce_destination_filepath)
+        txt_tce_dest.bind('<FocusOut>', self.entry_update)
+        txt_tce_dest.grid(row=4, column=1, padx=5, pady=5, sticky="NSEW")
+
+        lbl_tce_shoppinglist = ttk.Label(blk_options, text='Shopping list file (read only):')
+        lbl_tce_shoppinglist.grid(row=5, column=0, padx=5, pady=5, sticky="NSEW")
+        txt_tce_shoppinglist = ttk.Entry(blk_options, textvariable=self.var_tce_shoppinglist_path)
+        txt_tce_shoppinglist.bind('<FocusOut>', self.entry_update)
+        txt_tce_shoppinglist.grid(row=5, column=1, padx=5, pady=5, sticky="NSEW")
+
+        # Control
+        blk_control = ttk.LabelFrame(tab, text="Control")
+        blk_control.grid(row=3, column=0, padx=5, pady=5, sticky="NSEW")
+
+        btn_load_tce = ttk.Button(blk_control, text='Load TCE Destination', command=self.load_tce_dest)
+        btn_load_tce.grid(row=1, column=0, padx=5, pady=5, sticky="W")
+        lbl_load_tce = ttk.Label(blk_control, text='The current TCE destination will be loaded to the Single Waypoint fields on the debug page.')
+        lbl_load_tce.grid(row=1, column=1, padx=5, pady=5, sticky="EW")
+
+        separator = ttk.Separator(blk_control, orient='horizontal')
+        separator.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="EW")
+
+        btn_write_shopping = ttk.Button(blk_control, text='Write Global Shopping List', command=self.write_shopping_list)
+        btn_write_shopping.grid(row=3, column=0, padx=5, pady=5, sticky="W")
+        lbl_write_shopping = ttk.Label(blk_control, text="Writes the Global Shopping List for importing into the TCE shopping panel.")
+        lbl_write_shopping.grid(row=3, column=1, padx=5, pady=5, sticky="EW")
+
+    def entry_update(self, event):
+        self.tce_installation_path = str(self.var_tce_installation_path.get())
+        self.tce_shoppinglist_path = self.tce_installation_path + "\\SHOP\\ED_AP Shopping List.tsl"
+        self.tce_destination_filepath = str(self.var_tce_destination_filepath.get())
+
+        self.ap.config['TCEInstallationPath'] = str(self.var_tce_installation_path.get())
+        self.ap.config['TCEDestinationFilepath'] = str(self.var_tce_destination_filepath.get())
+
+    def load_tce_dest(self):
+        filename = self.ap.config['TCEDestinationFilepath']
+        if os.path.exists(filename):
+            with open(filename, 'r') as json_file:
+                f_details = json.load(json_file)
+
+            if self.ap_gui:
+                self.ap_gui.single_waypoint_system.set(f_details['StarSystem'])
+                self.ap_gui.single_waypoint_station.set(f_details['Station'])
+
+    @staticmethod
+    def goto_tce_webpage():
+        webbrowser.open_new("https://forums.frontier.co.uk/threads/trade-computer-extension-mk-ii.223056/")
 
 
 def dummy_cb(msg, body=None):
